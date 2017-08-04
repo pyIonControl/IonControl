0ce78fcbb5d0b498c65619201bd2eda37aee298a
Peter Maunz <plmaunz@sandia.gov>
2017-08-04 09:22:47 -0600

    add handshake between pulser data producing thread and event loop to make sure the event loop is not flooded with data can can still operate a snappy ui

--- pulser/PulserHardwareClient.py
+++ pulser/PulserHardwareClient.py
@@ -6,23 +6,23 @@
 """
 Encapsulation of the Pulse Programmer Hardware 
 """
-from queue import Queue
 import logging
 import multiprocessing
-from multiprocessing.sharedctypes import Array
 from ctypes import c_longlong
-from time import time
+from multiprocessing.sharedctypes import Array
+from queue import Queue
+from threading import Condition
 
 import numpy
-
 from PyQt5 import QtCore
+from PyQt5.QtCore import QTimer
 
 from modules.quantity import Q
-from .ServerProcess import FinishException
-from pulser.OKBase import ErrorMessages, FPGAException
-from .PulserHardwareServer import PulserHardwareServer
-from pulser.PulserHardwareServer import PulserHardwareException
 from pulser.LoggingReader import LoggingReader
+from pulser.OKBase import ErrorMessages, FPGAException
+from pulser.PulserHardwareServer import PulserHardwareException
+from .PulserHardwareServer import PulserHardwareServer
+from .ServerProcess import FinishException
 
 
 def check(number, command):
@@ -31,19 +31,20 @@ def check(number, command):
 
 
 class QueueReader(QtCore.QThread):      
-    def __init__(self, pulserHardware, dataQueue, parent = None):
+    def __init__(self, pulserHardware, dataQueue, condition_var, parent = None):
         QtCore.QThread.__init__(self, parent)
         self.exiting = False
         self.pulserHardware = pulserHardware
         self.running = False
         self.dataMutex = QtCore.QMutex()           # protects the thread data
         self.dataQueue = dataQueue
+        self.condition_var = condition_var
         self.dataHandler = { 'Data': lambda data, size : self.pulserHardware.dataAvailable.emit(data, size),
                              'DedicatedData': lambda data, size: self.pulserHardware.dedicatedDataAvailable.emit(data),
                              'FinishException': lambda data, size: self.raise_(FinishException()),
-                             'LogicAnalyzerData': lambda data, size: self.onLogicAnalyzerData(data) }
+                             'LogicAnalyzerData': self.onLogicAnalyzerData}
    
-    def onLogicAnalyzerData(self, data): 
+    def onLogicAnalyzerData(self, data, size):
         self.pulserHardware.logicAnalyzerDataAvailable.emit(data)
         
     def raise_(self, ex):
@@ -55,7 +56,14 @@ class QueueReader(QtCore.QThread):
         while True:
             try:
                 data = self.dataQueue.get()
-                self.dataHandler[data.__class__.__name__](data, time())  # self.dataQueue.qsize()
+                self.dataHandler[data.__class__.__name__](data, self.dataQueue.qsize())
+                #  use condition_var to wait until gui thread has processed data and all other events
+                #  this generates a handshake with the event loop, we send out a signal to the event loop
+                #  the signal gets added to the end of the queue, once it is processed, it signals back to this thread
+                #  that the next data element can be submitted
+                with self.condition_var:
+                    self.pulserHardware.next_data_trigger.emit()
+                    self.condition_var.wait()
             except (KeyboardInterrupt, SystemExit, FinishException):
                 break
             except Exception:
@@ -72,6 +80,7 @@ class PulserHardware(QtCore.QObject):
     logicAnalyzerDataAvailable = QtCore.pyqtSignal( 'PyQt_PyObject' )
     shutterChanged = QtCore.pyqtSignal( 'PyQt_PyObject' )
     ppActiveChanged = QtCore.pyqtSignal( object )
+    next_data_trigger = QtCore.pyqtSignal()
     
     timestep = Q(5, 'ns')
 
@@ -93,14 +102,20 @@ class PulserHardware(QtCore.QObject):
         self.serverProcess = self.serverClass(self.dataQueue, self.serverPipe, self.loggingQueue, self.sharedMemoryArray )
         self.serverProcess.start()
 
-        self.queueReader = QueueReader(self, self.dataQueue)
+        self.condition_var = Condition()
+        self.next_data_trigger.connect(lambda: QTimer.singleShot(0, self.next_data_notify))
+        self.queueReader = QueueReader(self, self.dataQueue, self.condition_var)
         self.queueReader.start()
         
         self.loggingReader = LoggingReader(self.loggingQueue)
         self.loggingReader.start()
         self.ppActive = False
         self._pulserConfiguration = None
-        
+
+    def next_data_notify(self):
+        with self.condition_var:
+            self.condition_var.notifyAll()
+
     def shutdown(self):
         self.clientPipe.send(('finish', (), {}))
         self.serverProcess.join()
