0f98976b0a0ef50d2db9b6b03fec93f1a3fadf48
Peter Maunz <plmaunz@sandia.gov>
2017-08-18 15:05:57 -0600

    make filename commandline parameter

--- extractGST.py
+++ extractGST.py
@@ -1,11 +1,18 @@
+import argparse
+
 import pygsti
 from pygsti import report
 from pygsti.construction import std1Q_XYI
 
 from trace.TraceCollection import TraceCollection
 
+parser = argparse.ArgumentParser(description='Parametrically generate Phoenix geometry')
+parser.add_argument('--filename', type=str, default="Phoenix", help='filename of voltage array')
+args = parser.parse_args()
+filename = args.filename[0]
+
 Trace = TraceCollection()
-Trace.loadZip(r'C:\Users\plmaunz\Documents\170807\GSTScan_057.zip')
+Trace.loadZip(filename)
 qubitData = Trace.structuredData['qubitData']
 ds = qubitData.gst_dataset
 my_gs_target = qubitData.target_gateset
@@ -13,7 +20,7 @@ gs_target = std1Q_XYI.gs_target
 gs_target.spamdefs = my_gs_target.spamdefs
 
 
-pygsti.io.write_dataset(r'C:\Users\plmaunz\Documents\170807\GSTScan_057_qubitdata.txt', ds, spamLabelOrder=['0', '1'])
+pygsti.io.write_dataset('qubitdata.txt', ds, spamLabelOrder=['0', '1'])
 
 #Create fiducial gate string lists
 fiducials = pygsti.construction.gatestring_list( [ (), ('Gx',), ('Gy',), ('Gx','Gx'), ('Gx','Gx','Gx'), ('Gy','Gy','Gy') ])
@@ -32,5 +39,5 @@ maxLengths = [1,2,4,8,16,32,64,128,256,512,1024]
 
 #gs_target.set_all_parameterizations("TP")
 results = pygsti.do_stdpractice_gst(ds, gs_target, prep_fiducials, meas_fiducials, germs, maxLengths)
-pygsti.report.create_general_report(results, filename=r'C:\Users\plmaunz\Documents\170807\GSTScan_057.html',
+pygsti.report.create_general_report(results, filename=r'output.html',
                                     title="GSTScan_057", verbosity=2)
