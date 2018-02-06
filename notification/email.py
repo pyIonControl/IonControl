4876c963a7cd69ac0eaf1cd4c3352210b441269a
Peter Maunz <plmaunz@sandia.gov>
2018-02-06 12:46:56 -0700

    notification by email

---
+++ notification/email.py
@@ -0,0 +1,15 @@
+import getpass
+import smtplib
+
+data_template = "From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n{}\r\n"
+
+class EmailNotification:
+    def __init__(self, domain):
+        self.server = "smtp." + domain
+        self.sender = "{}@{}".format(getpass.getuser(), domain)
+
+    def message(self, recipients, title, message, priority=None):
+        server = smtplib.SMTP(self.server)
+        DATA = data_template.format(self.sender, ", ".join(recipients), title, message)
+        response = server.sendmail(self.sender, recipients, DATA)
+        return response
