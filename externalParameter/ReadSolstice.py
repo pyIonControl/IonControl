4a4495025c48e93a58b99ff7773bbebc50f5d0e8
SANDIA\calcium <calcium@sandia.gov>
2018-05-03 09:24:01 -0600

    add code to read Solstice

---
+++ externalParameter/ReadSolstice.py
@@ -0,0 +1,23 @@
+from websocket import create_connection
+import json
+from influxdb import InfluxDBClient
+
+ws = create_connection("ws://192.168.1.222:8088/index.htm")
+client = InfluxDBClient('r446212.mdl.sandia.gov', 8086, 'root', 'root', 'lab')
+
+while True:
+    msg = json.loads(ws.recv())
+    if msg['message_type'] == "left_panel":
+        msg.pop('message_type')
+        msg.pop('browser_tab')
+        tags = {key: value for key, value in msg.items() if type(value) == str}
+        fields = {key: value for key, value in msg.items() if type(value) == float}
+        json_body = [
+             {
+                 "measurement": "solstice_2220",
+                 "tags": tags,
+                 "fields": fields,
+            }
+        ]
+        client.write_points(json_body)
+        print(msg['lp_power'])
